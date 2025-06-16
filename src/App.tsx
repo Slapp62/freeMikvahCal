c5147c37ef80240d590db413745c09d879c96256
Simcha <slapp62@gmail.com>
2025-06-16 13:43:50 +0300

    register page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
+import { Notifications } from '@mantine/notifications';
 import { theme } from "./theme";
 import { AppRouter } from "./routing/AppRouter";
 import { AuthProvider } from "./context/AuthProvider";
+
 
 export default function App() {
   return (
     <AuthProvider>
         <MantineProvider theme={theme}>
+            <Notifications/>
             <AppRouter/>
         </MantineProvider> 
     </AuthProvider>);
--- src/pages/register.page.tsx
+++ src/pages/register.page.tsx
@@ -1,53 +1,86 @@
-import { useState } from "react";
+import { FieldValues, useForm,  } from "react-hook-form";
 import { Button, Stack, TextInput, Title } from "@mantine/core";
 import { supabase } from "../lib/supabaseClient";
+import { notifications } from '@mantine/notifications';
+import '@mantine/notifications/styles.css';
 
 const RegisterPage = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleRegister = async (e: React.FormEvent) => {
-    e.preventDefault();
-
-    const { error } = await supabase.auth.signUp({
-      email,
-      password,
-    });
-
-    if (error) {
-      console.error("Sign up error:", error.message);
-    } else {
-      console.log("Sign up successful, check your email to confirm.");
-      // optionally clear form
-      setEmail("");
-      setPassword("");
+    type RegisterValues = {
+        email: string;
+        password: string;
     }
-  };
+
+    const {register, handleSubmit, reset, formState: {errors}} = useForm<RegisterValues>(
+        {
+            mode: 'onChange',
+        }
+    );
+
+    const onSubmit = async (data : FieldValues) => {
+    
+        const { error } = await supabase.auth.signUp({
+        email: data.email,
+        password: data.password
+        });
+
+        if (error) {
+            notifications.show({
+                color: "red", 
+                title: "Sign up error:", 
+                message: error.message})
+        } else {
+            notifications.show({
+                color: "green", 
+                title: "Success!", 
+                message: "Check your email to confirm your account."})
+            reset();
+        }
+    };
 
   return (
-    <Stack>
-      <Title>Register</Title>
-      <form onSubmit={handleRegister}>
+    <Stack w='80%' mx='auto'>
+      <Title order={1} ta="center">Register</Title>
+      
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Stack w='40%' mx='auto' gap={20}>
         <TextInput
           label="Email"
           placeholder="you@mantine.dev"
           required
-          value={email}
-          onChange={(e) => setEmail(e.currentTarget.value)}
+          error={errors.email?.message}
+          {...register("email", {
+                required: "Email is required",
+                pattern: {
+                    value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                    message: "Invalid email format"  
+                }
+            }
+          )}
         />
 
         <TextInput
           label="Password"
           placeholder="Your password"
           required
-          mt="md"
           type="password"
-          value={password}
-          onChange={(e) => setPassword(e.currentTarget.value)}
+          error={errors.password?.message}
+          {...register("password", {
+                required: "Password is required",
+                minLength: {
+                    value: 8,
+                    message: "Must be at least 8 characters"
+                },
+                pattern: {
+                    value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+                    message: "Must contain at least one letter and one number"
+                }
+            })}
         />
 
         <Button type="submit">Register</Button>
+        </Stack>
       </form>
+      
     </Stack>
   );
 };
